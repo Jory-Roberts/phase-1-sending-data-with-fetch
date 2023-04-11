17ba852f1464f58d1d2fb543c6e337edb60aa768
Jory Roberts <robertsjory13@gmail.com>
2023-04-11 18:19:52 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+const name = document.getElementById("name");
+
+const formData = {
+  name: "Steve",
+  email: "steve@steve.com",
+};
+
+const configurationObject = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify(formData),
+};
+
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then(response => response.json())
+      .then(data => {
+          const newId = data.id;
+          const newIdElement = document.createElement('p');
+          newIdElement.textContent = `${newId}`;
+          document.body.appendChild(newIdElement);
+      })
+      .catch(error => {
+          const errorElement = document.createElement('p');
+          errorElement.textContent = `Error: ${error}`;
+          document.body.appendChild(errorElement);
+   })
+}
